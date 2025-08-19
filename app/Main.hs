d5ea8f4d64afe20b444ffb472466db43f5bf9326
permalik <permalik@protonmail.com>
2025-08-19 18:16:18 -0500

    feat: impl structured logging

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,9 @@ module Main where
 import Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Format (defaultTimeLocale, formatTime)
 import GHC.Generics (Generic)
 import Network.HTTP.Simple
 import Network.HTTP.Types (methodPost, status200, status400)
@@ -21,22 +24,37 @@ data Payload = Payload
 instance FromJSON Payload
 instance ToJSON Payload
 
+data LogEntry = LogEntry
+    { timestamp :: Text
+    , message :: Text
+    }
+    deriving (Show, Generic)
+
+instance ToJSON LogEntry
+
+logMsg :: Text -> IO ()
+logMsg msg = do
+    now <- getCurrentTime
+    let ts = T.pack $ formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%QZ" now
+        entry = LogEntry ts msg
+    BL.putStrLn $ encode entry
+
 app :: W.Application
 app req respond
     | W.requestMethod req == methodPost && W.rawPathInfo req == "/" = do
         body <- W.strictRequestBody req
         case decode body :: Maybe Payload of
             Just payload -> do
-                putStrLn "Received and decoded JSON:"
-                print payload
+                logMsg "Received and decoded JSON:"
+                logMsg (T.pack (show payload))
 
                 let newJson = encode payload
                 _ <- postToOtherService newJson
-                putStrLn "Forwarded to external service."
+                logMsg "Forwarded to external service."
 
                 respond $ W.responseLBS status200 [("Content-Type", "application/json")] newJson
             Nothing -> do
-                putStrLn "Failed to decode JSON."
+                logMsg "Failed to decode JSON."
                 respond $ W.responseLBS status400 [("Content-Type", "text/plain")] "Invalid JSON"
     | otherwise = respond $ W.responseLBS status400 [("Content-Type", "text/plain")] "Bad Request"
 
@@ -51,5 +69,5 @@ postToOtherService json = do
 
 main :: IO ()
 main = do
-    putStrLn "Running server on http://localhost:9999"
+    logMsg "Running server on http://localhost:9999"
     run 9999 app
